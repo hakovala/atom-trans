7299315c8177429f9b68b2f62e342b33cb98d399
Harri Kovalainen <hakovala@gmail.com>
2016-10-15 17:06:27 +0300

    Add project base
    This includes gulp build system and unit test system
    using `electron-mocha`.
    
    Signed-off-by: Harri Kovalainen <hakovala@gmail.com>

---
+++ gulp/unit-tests.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const path = require('path');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const mochelec = require('gulp-mochelec');
+const notifier = require('node-notifier');
+const notify = require('gulp-notify');
+const through = require('through2');
+
+const TIMEOUT_PASS = 2000;
+const TIMEOUT_FAIL = 4000;
+const IMAGE_PASS = path.join(__dirname, 'img/pass.png');
+const IMAGE_FAIL = path.join(__dirname, 'img/fail.png');
+
+function notifyFailure(err) {
+	gutil.log("Failure: " + err.message);
+	notifier.notify({
+		'expire-time': TIMEOUT_FAIL,
+		icon: IMAGE_FAIL,
+		title: "Failed",
+		message: err.message
+	});
+}
+
+function notifyPass() {
+	return through.obj((file, enc, cb) => {	
+		notifier.notify({
+			'expire-time': TIMEOUT_PASS,
+			icon: IMAGE_PASS,
+			title: "Pass",
+			message: "All unit tests passed."
+		});
+		cb(null, file);
+	});
+}
+
+gulp.task('test', () => {
+	gulp.src('./test/test-dom-util.js', { read: false })
+		.pipe(mochelec({
+			renderer: true,
+		}))
+		.on('error', notifyFailure)
+		.pipe(notifyPass())
+});
+
+gulp.task('test:watch', ['test'], () => {
+	gulp.watch(['index.js', './lib/**/*.js', './test/*.js'], ['test']);
+});
