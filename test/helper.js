143cd542ecc6af8deb2b1e8f74821a05e149edc3
Harri Kovalainen <hakovala@gmail.com>
2016-10-20 20:38:46 +0300

    Refactor test helper module
    Signed-off-by: Harri Kovalainen <hakovala@gmail.com>

--- test/helper.js
+++ test/helper.js
@@ -1,37 +1,39 @@
 "use strict";
 
-function elToString(el) {
-	let desc;
-
-	if (el instanceof NodeList || el instanceof HTMLCollection) {
-		if (el.length === 0) return 'empty NodeList';
-		desc = Array.prototype.slice.call(el, 0, 5)
-			.map(elToString).join(', ');
-		return el.length > 5 ? desc + '... (+' + (el.length - 5) + ' more)' : desc;
-	}
-
-	if (!(el instanceof HTMLElement)) {
-		return String(el);
-	}
-
-	desc = el.tagName.toLowerCase();
-	if (el.id) {
-		desc += '#' + el.id;
-	}
-	if (el.className) {
-		desc += '.' + String(el.className).replace('\s+', '.');
-	}
-	Array.prototype.forEach.call(el.attributes, (attr) => {
+function elementToString(el) {
+	let str = el.tagName.toLowerCase();
+	if (el.id) str += `#${el.id}`;
+	if (el.className) str += `.${String(el.className).replace('\s+', '.')}`;
+	Array.from(el.attributes, (attr) => {
 		if (attr.name !== 'class' && attr.name !== 'id') {
-			desc += '[' + attr.name + (attr.value ? '="' + attr.value + '"]' : ']');
+			str += `[${attr.name}${attr.value && '="' + attr.value + '"'}]`;
 		}
 	});
-	return desc;
+	return `[${str}]`;
+}
+
+function arrayToString(arr) {
+	if (arr.length === 0) return 'empty Array';
+	let str = Array.from(arr).slice(0, 5)
+		.map(toString).join(', ');
+	return arr.length > 5 ? str + `... (${el.length - 5} more)` : str;
+}
+
+function toString(obj) {
+	if (obj instanceof NodeList ||
+		obj instanceof HTMLCollection ||
+		Array.isArray(obj)) {
+		return arrayToString(obj);
+	} else if (obj instanceof HTMLElement) {
+		return elementToString(obj);
+	} else {
+		return 'unknonw';
+	}
 }
 
 module.exports = {
-	toString: elToString,
+	toString: toString,
 	inspect: function(el) {
-		console.log(elToString(el));
+		console.log(toString(el));
 	}
 };
