6ba920d59e3c87be1823105d13bb3cccad39b2a4
Harri Kovalainen <harri@augumenta.com>
2019-01-28 12:04:18 +0200

    Add index API entry point
    Signed-off-by: Harri Kovalainen <harri@augumenta.com>

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 "use strict";
 
-// TODO: Add implementation
+const dom = require('./lib/dom-util');
+const Widget = require('./lib/widget');
+const WidgetEvent = require('./lib/widget-event');
+
+const trans = function(selector, parent) {
+	return Widget.query(selector, parent);
+};
+
+trans.all = function(selector, parent) {
+	return Widget.queryAll(selector, parent);
+};
+trans.dom = dom;
+trans.Widget = Widget;
+trans.Event = WidgetEvent;
+
+module.exports = trans;
